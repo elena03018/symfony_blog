8c2aec866dc4c99cbc6741ab6e1b6702fb87da0c
elena03018 <elenabellu85@gmail.com>
2024-05-07 17:02:03 +0200

    sprint2_ticket1_feat: Registration system implemented

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'Impossible de créer un compte avec cet email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
 
@@ -17,17 +20,55 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
+    //prénom
+    #[Assert\NotBlank(message: "Le prénom est obligatoire.")]
+    #[Assert\Length(
+    max: 255,
+    maxMessage: 'Le prénom ne doit pas dépasser {{ limit }} caracteres',
+    )]
 
+
+    #[Assert\Regex(
+        pattern: "/^[0-9a-zA-Z-_' áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+$/i",
+        match: true,
+        message: 'Seuls les chiffres, les lettres, l underscore et tiret sont autorisés.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $firstName = null;
 
 
+    //nom 
+    #[Assert\NotBlank(message: "Le nom est obligatoire.")]
+    #[Assert\Length(
+    max: 255,
+    maxMessage: 'Le nom ne doit pas dépasser {{ limit }} caracteres',
+    )]
+
+
+    #[Assert\Regex(
+        pattern: "/^[0-9a-zA-Z-_' áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+$/i",
+        match: true,
+        message: 'Seuls les chiffres, les lettres, l underscore et tiret sont autorisés.',
+    )]
     #[ORM\Column(length: 255)]
     private ?string $lastName = null;
 
 
-    #[ORM\Column(length: 180)]
+    //email
+    #[Assert\NotBlank(message: "L email est obligatoire.")]
+    #[Assert\Length(
+    max: 180,
+    maxMessage: 'L email ne doit pas dépasser {{ limit }} caracteres',
+    )]
+
+    #[Assert\Email(message: 'L email {{ value }} est invalide.',
+    )]
+    
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
+
+
+
 
 
     /**
@@ -40,6 +81,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      */
+    #[Assert\NotBlank(message: "Le mot de passe est obligatoire.")]
+    #[Assert\Length(
+    min: 12,
+    max: 255,
+    minMessage: 'Le mot de passe doit contenir au minimum {{ limit }} caractères',
+    maxMessage: 'Le mot de passe doit contenir au minimum {{ limit }} caractères',
+    )]
+    #[Assert\Regex(
+        pattern: "/^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{11,255}$/",
+        match: true,
+        message: "Le mot de passe doit contentir au moins une lettre miniscule, majuscule, un chiffre et un caractère spécial.",
+    )]
     #[ORM\Column]
     private ?string $password = null;
 
@@ -51,6 +104,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
     
 
     public function getId(): ?int
@@ -172,6 +228,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
